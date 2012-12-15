f5656c362308daccfe93dc2218b30fb25a9d08b4
Tobias Leich <email@froggs.de>
2012-12-15 19:52:22 +0100

    read from fs-structure in libs/, using META.info-like structure for MANIFESTs

--- lib/S11Versioning.pm
+++ lib/S11Versioning.pm
@@ -3,6 +3,11 @@ module S11Versioning;
 
 use S11Versioning::Grammar;
 
+use JSON::Tiny;
+
+my @dists = ();
+@dists.push: from-json( slurp "{%*CUSTOM_LIB{$_}}/MANIFEST" ).flat if "{%*CUSTOM_LIB{$_}}/MANIFEST".IO.e for <home site vendor perl>;
+
 my $dists = (
 	{
 		name => 'Perl',
@@ -18,29 +23,30 @@ my $dists = (
 
 sub use ( $module ) is export {
 	if my $bag = S11Versioning::Grammar.parse( $module ) {
-		say 'Whooohoo! ' ~ $module;
+		#say 'Whooohoo! ' ~ $module;
 
-		for $dists.values -> $dist {
+		for @dists.values -> $dist {
+			say $dist;
 			next unless $bag<name> eq $dist<name>;
 
 			if $bag<auth>[0]<name> {
 				next unless $bag<auth>[0]<name> eq 'Any'
 				         || $bag<auth>[0]<name> eq '*'
-				         || $bag<auth>[0]<name> eq $dist<auth>
+				         || $bag<auth>[0]<name> eq $dist<author>
 			}
 			elsif $bag<auth>[0]<url> {
-				next unless $bag<auth>[0]<url> eq $dist<auth>
+				next unless $bag<auth>[0]<url> eq $dist<author>
 			}
 			elsif $bag<auth>[0]<code> {
 				my $s = '-> $_ {' ~ $bag<auth>[0]<code> ~ '}';
-				next unless (eval $s)( $dist<auth> );
+				next unless (eval $s)( $dist<author> );
 			}
 			elsif $bag<auth>[0]<regex> {
-				next unless $dist<auth> ~~ /<{$bag<auth>[0]<regex>}>/;
+				next unless $dist<author> ~~ /<{$bag<auth>[0]<regex>}>/;
 			}
 
 			if $bag<ver>[0] {
-				my $ver = Version.new( ~$dist<ver> );
+				my $ver = Version.new( ~$dist<version> );
 
 				if $bag<ver>[0]<version_from> && $bag<ver>[0]<version_to> {
 					my $from = Version.new( ~$bag<ver>[0]<version_from> );
@@ -53,10 +59,10 @@ sub use ( $module ) is export {
 				}
 			}
 
-			say $dist
+			#say $dist
 		}
 	}
 	else {
-		say 'Dope -.- ' ~ $module;
+		#say 'Dope -.- ' ~ $module;
 	}
 }
